89435059eadbb9b6795e032cabca3df6f865049a
raph5 <raphguyader@gmail.com>
2024-06-13 17:31:00 +0200

    run1 : ajout de l'inertie

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import sys
 
 WIDTH = 100
 HEIGHT = 100
-VITESSE = 1 / WIDTH
+VITESSE = 0.1 / WIDTH
+INERTIE = 0.6
 
 
 def main() :
@@ -15,26 +16,30 @@ def main() :
 
     # createReference()
 
-    referenceImage = loadImage("run0/reference.png")
+    referenceImage = loadImage("run1/reference.png")
     params = Vector([ Var(0), Var(0), Var(0) ])
+    delta_params = [0.0, 0.0, 0.0]
 
-    for i in range(10) :
+    for i in range(20) :
         image = render(params, WIDTH, HEIGHT)
-        saveImage(image, f"run0/render{i}.png", WIDTH, HEIGHT)
+        saveImage(image, f"run1/render{i}.png", WIDTH, HEIGHT)
         loss = (referenceImage - image).norme()
         loss.retour()
 
         print("loss :", loss.valeur)
-        print("gradient :", [ c.adjoint for c in params ])
+        # print("gradient :", [ c.adjoint for c in params ])
+        # print("delta :", delta_params)
 
-        for p in params :
-            p.valeur -= VITESSE * p.adjoint
+        for i in range(len(params)) :
+            delta_p = INERTIE * delta_params[i] + VITESSE * params[i].adjoint
+            params[i].valeur -= delta_p
+            delta_params[i] = delta_p
 
 
 def createReference() :
     params = Vector([ Var(1), Var(0.6), Var(0.4) ])
     image = render(params, WIDTH, HEIGHT)
-    saveImage(image, "run0/reference.png", WIDTH, HEIGHT)
+    saveImage(image, "run1/reference.png", WIDTH, HEIGHT)
 
 
 if __name__ == "__main__" :
